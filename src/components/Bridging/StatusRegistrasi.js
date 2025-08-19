1943cad92141f4b13cd5dd87c58e2711d00f5c48
amiga11 <bimaamg@gmail.com>
2025-08-19 15:26:15 +0700

    update l

--- src/components/Bridging/StatusRegistrasi.js
+++ src/components/Bridging/StatusRegistrasi.js
@@ -225,7 +225,7 @@ const StatusRegistrasi = () => {
           <ActionButtonDisabled label="Permohonan Gagal Verifikasi Email Kadaluarsa" disabled />
         </div>
       );
-    }else if (value.status_verifikasi === "terverifikasi" && value.status_pendaftaran === "tunda") {
+    } else if (value.status_verifikasi === "terverifikasi" && value.status_pendaftaran === "tunda") {
 
       return (
         <div style={{ display: "flex", alignItems: "center", width: "100%" }}>
